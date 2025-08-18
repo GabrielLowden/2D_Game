de2af7e643de54bbc2f983053757ab25f9553a8a
Gabriel Lowden <98785105+GabeLowden@users.noreply.github.com>
2025-08-17 21:25:17 -0700

    Starting on Game Menu Logic.

--- simple2dGame/main.cpp
+++ simple2dGame/main.cpp
@@ -1,9 +1,18 @@
-#include <SFML/Graphics.hpp>
 #include "Menu.hpp"
+#include <string>
 
 int main()
 {
-   
+	std::string title = "Welcome to....";
+	unsigned int x = 800;
+	unsigned int y = 600;
+	menu mainMenu(x, y, title);
+
+	while (mainMenu.isOpen()) {
+	//add window events...
+
+	}
+
 
 	return 0;
 }
