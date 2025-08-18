489d94b17538a842c92e039c2125f41323840133
Gabriel Lowden <98785105+GabeLowden@users.noreply.github.com>
2025-08-17 18:10:26 -0700

    Updating Menu.hpp with more "menu" class function declarations.

--- simple2dGame/Menu.hpp
+++ simple2dGame/Menu.hpp
@@ -5,7 +5,16 @@ class menu {
 		//constructor
 		menu(int width, int height) {};
 
+		void draw(sf::RenderWindow& window);
+		void handleKeyboardInput(sf::Event event);
+		void handleMouseInput(sf::Vector2f mousePos);
+		const int getPressedItem();
+		void addItem(const std::string& text);
+		void setBackgroundTexture(const std::string& filename);
+		void setSelectionSound(const std::string& filename);
+		void animateSelection();
 
-
+	private:
+		sf::Font font;
 
 };
